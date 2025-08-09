173ecb24236a2ffbbc32f6ccf5129462d1af0cb5
yago.vidal1101 <yago.vidal1101@gmail.com>
2025-08-09 19:54:53 +0000

    Pronto

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { use, useState } from "react"
+export function App() {
+  const[numero1, setNumero1] = useState(0)
+  const[numero2, setNumero2] = useState(0)
+  const[resultado, setResultado] = useState(0)
+  const[jaSomei, setJaSomei] = useState(false)
+  function somar(e) {
+    e.preventDefault()
+    setResultado(numero1 + numero2)
+    if (jaSomei === false) {
+      setJaSomei(true)
+    }
+  }
+  return(
+    <main>
+      <h1> Calculadora no react</h1>
+
+      <form action="">
+        <input type="number" placeholder="Numero 1" onChange={(e) => setNumero1(e.target.valueAsNumber)} />
+        <input type="number" placeholder="Numero 2" onChange={(e) => setNumero2(e.target.valueAsNumber)}/>
+        <button onClick={somar}>Somar</button>
+      </form>
+      {jaSomei && <p>Resultado: {resultado}</p>}
+    </main>
+  )
+}
